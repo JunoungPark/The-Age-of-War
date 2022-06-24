a3dc59fd04e6beaa8967090a5ed6df3ea1f16dcd
1 <1>
2022-06-24 21:46:42 +0900

    Unity Class 2022-06-24

--- The Age of War/Assets/Script/Control.cs
+++ The Age of War/Assets/Script/Control.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
+
+    //RaycastHit hit;
+    //
+    //Ray ray;
+
     int damage = 10;
+
     public float speed = 1.0f;
+
+    public int health;
 
     Animator animator;
     // Start is called before the first frame update
@@ -16,21 +24,48 @@ public class Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
         if (animator.GetBool("Attack") == true)
         {
             if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
             {
-                animator.SetBool("Attack", false);
+                health -= 10;
+                animator.Rebind();
             }
         }
         else
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        if(health <= 0)
+            Destroy(gameObject);
+        
+        
+        //ray = new Ray(transform.position,transform.forward);
+        //
+        //if (Physics.Raycast(ray, out hit, 1.5f))
+        //{
+        //
+        //    animator.SetBool("Attack", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("Attack", false);
+        //    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //
+        //}
+        
     }
     private void OnTriggerStay(Collider other)
     {
+
         if (other.tag == "Enemy" || other.tag == "Player")
         {
             animator.SetBool("Attack", true);
         }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("Attack", false);
     }
 }
--- The Age of War/Assets/Script/CreateMonster.cs
+++ The Age of War/Assets/Script/CreateMonster.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class CreateMonster : MonoBehaviour
 {
+
+    float time;
+
+    int gold;
+    
     // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("EnemyInstance", 0, 5);
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 1)
+        {
+            time--;
+            gold++;
+        }
+    }
     public void Create()
     {
         Instantiate(Resources.Load<GameObject>("Warrior Goblin"), new Vector3(-34, 0, 50), Quaternion.Euler(0, 90, 0));
     }
     // Update is called once per frame
-   
+   public void EnemyInstance()
+    {
+        if (gold >= 1)
+        {
+            Instantiate(Resources.Load<GameObject>("Enemy Warrior Goblin"), new Vector3(34, 0, 50), Quaternion.Euler(0, -90, 0));
+        }
+    }
 }
